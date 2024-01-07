2742202eb58b4ae6c5e262256b12eef7200f2d15
RanijRC <raazchy82@gmail.com>
2024-01-07 09:08:41 +0545

    APIInitiate

--- CRMS.Blazor/Program.cs
+++ CRMS.Blazor/Program.cs
@@ -1,16 +1,23 @@
-using CRMS.Application.Common.Interface;
 using CRMS.Blazor.Components;
-using CRMS.Infrastructure.Services;
+using CRMS.Application.Common.Interface;
 using CRMS.Infrastructure.DI;
+using CRMS.Infrastructure.Services;
+using CRMS.Blazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
-builder.Services.AddScoped<IAuthentication, AuthenticationService>();
+
+
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddAuthentication();
+builder.Services.AddScoped<IAuthentication, AuthenticationService>();
+builder.Services.AddHttpClient();
+builder.Services.AddTransient<ApiService>();
+
+
 
 var app = builder.Build();
 
@@ -21,7 +28,6 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
 
 app.UseRouting(); //for identity
 app.UseAuthentication(); //for identity
